d1e21007e08f4f9cf5274487969a49d6dcdd18ec
moaidshurafa <moayada124@gmail.com>
2024-08-19 18:25:25 +0300

    change

---
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        // T - category
+        IEnumerable<T> GetAll();//retrieve all the category where we are displaying all of them.
+        T Get(Expression<Func<T,bool>> filter);//retrieve only one category, you dont need to understand this put just write it like this.
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
